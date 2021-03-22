a441cdca1f5684ba34168e050bd9454df7855bcf
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2021-03-22 15:17:26 +0100

    image upload endpoint added

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv");
+const path = require("path");
 const connectDB = require("./config/db");
 const productRouter = require("./router/productRoutes");
 const userRouter = require("./router/userRoutes");
 const orderRouter = require("./router/orderRoutes");
+const uploadRouter = require("./router/uploadRoutes");
 const errorMiddleware = require("./middleware/errorMiddleware.js");
 
 dotenv.config();
@@ -16,10 +18,13 @@ app.use(express.json());
 app.use("/api/products", productRouter);
 app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
+app.use("/api/upload", uploadRouter);
 
 app.use("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
+
+app.use("/public", express.static(path.join(__dirname, "/public")));
 
 app.use(errorMiddleware.notFound);
 app.use(errorMiddleware.errorHandler);
---
+++ router/uploadRoutes.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+
+const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, "public/images/");
+  },
+  filename(req, ifle, cb) {
+    cb(
+      null,
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+
+function checkFileType(file, cb) {
+  const filetypes = /jpg|jpeg|png/;
+  const extName = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimeType = filetypes.test(file.mimetype);
+
+  if (extName && mimeType) {
+    return cb(null, true);
+  } else {
+    cb("Images only!");
+  }
+}
+
+const upload = multer({
+  storage: storage,
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+});
+
+router.post("/", upload.single("image"), (req, res) => {
+  res.send(`/${req.file.path}`);
+});
+
+module.exports = router;
