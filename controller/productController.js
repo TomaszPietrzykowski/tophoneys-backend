9872a52deef3c9197a4aa5e16c440e5f358756f7
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2021-05-10 20:26:14 +0200

    pagination added to category and all products controllers

--- controller/productController.js
+++ controller/productController.js
@@ -1,50 +1,63 @@
-const Product = require("../model/productModel");
-const asyncHandler = require("express-async-handler");
-const fs = require("fs");
-const path = require("path");
+const Product = require("../model/productModel")
+const asyncHandler = require("express-async-handler")
+const fs = require("fs")
+const path = require("path")
 
 // @description: Fetch all products
 // @route: GET /api/products
 // @access: Public
 exports.getProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find({});
-  res.json(products);
-});
+  const pageSize = 2
+  const page = Number(req.query.pageNumber) || 1
+  const count = await Product.countDocuments({})
+  const products = await Product.find({})
+    .limit(pageSize)
+    .skip(pageSize * (page - 1))
+  res.json({ products, page, pages: Math.ceil(count / pageSize) })
+})
 
 // @description: Fetch single product
 // @route: GET /api/products/:id
 // @access: Public
 exports.getProductById = asyncHandler(async (req, res) => {
-  const product = await Product.findById(req.params.id);
+  const product = await Product.findById(req.params.id)
   if (product) {
-    res.json(product);
+    res.json(product)
   } else {
-    res.status(404).json({ message: "Product not found" });
+    res.status(404).json({ message: "Product not found" })
   }
-});
+})
 
 // @description:   Get products by category
 // @route:  GET /api/products/category/:id
 // @access: Public
 exports.getCategory = asyncHandler(async (req, res) => {
-  const id = req.params.id;
+  const pageSize = 3
+  const page = Number(req.query.pageNumber) || 1
+  const id = req.params.id
   if (id === "new") {
-    const products = await Product.find().sort({ createdAt: -1 }).limit(10);
-    res.json(products);
+    const count = await Product.countDocuments({})
+    const products = await Product.find({})
+      .sort({ createdAt: -1 })
+      .limit(pageSize)
+      .skip(pageSize * (page - 1))
+    res.json({ products, page, pages: Math.ceil(count / pageSize) })
   } else if (id === "sale") {
-    const products = await Product.find({ isPromo: true });
-    res.json(products);
+    const count = await Product.countDocuments({ isPromo: true })
+    const products = await Product.find({ isPromo: true })
+    res.json({ products, page, pages: Math.ceil(count / pageSize) })
   } else {
-    const products = await Product.find({ category: id });
-    res.json(products);
+    const count = await Product.countDocuments({ category: id })
+    const products = await Product.find({ category: id })
+    res.json({ products, page, pages: Math.ceil(count / pageSize) })
   }
-});
+})
 
 // @description:   Get products by keyword
 // @route:  GET /api/products/search/:keyword
 // @access: Public
 exports.searchProducts = asyncHandler(async (req, res) => {
-  const keyword = req.params.keyword;
+  const keyword = req.params.keyword
 
   if (keyword) {
     const products = await Product.find({
@@ -52,28 +65,28 @@ exports.searchProducts = asyncHandler(async (req, res) => {
         $regex: keyword,
         $options: "i",
       },
-    });
-    res.json(products);
+    })
+    res.json(products)
   } else {
-    const products = await Product.find({});
-    res.json(products);
+    const products = await Product.find({})
+    res.json(products)
   }
-});
+})
 
 // @description: Delete product
 // @route: DELETE /api/products/:id
 // @access: Private/Admin
 exports.deleteProduct = asyncHandler(async (req, res) => {
-  const product = await Product.findById(req.params.id);
+  const product = await Product.findById(req.params.id)
   if (product) {
-    const img = product.image;
-    fs.unlinkSync(path.join(__dirname, "../", img));
-    await product.remove();
-    res.json({ message: "Product removed" });
+    const img = product.image
+    fs.unlinkSync(path.join(__dirname, "../", img))
+    await product.remove()
+    res.json({ message: "Product removed" })
   } else {
-    res.status(404).json({ message: "Product not found" });
+    res.status(404).json({ message: "Product not found" })
   }
-});
+})
 
 // @description: Create product
 // @route: POST /api/products
@@ -93,39 +106,39 @@ exports.createProduct = asyncHandler(async (req, res) => {
     countryOfOrigin: req.body.countryOfOrigin,
     isPromo: req.body.isPromo,
     isPublished: req.body.isPublished,
-  });
+  })
 
-  const createdProduct = await product.save();
-  res.status(201).json(createdProduct);
-});
+  const createdProduct = await product.save()
+  res.status(201).json(createdProduct)
+})
 
 // @description: Update product
 // @route: PUT /api/products/:id
 // @access: Private/Admin
 exports.updateProduct = asyncHandler(async (req, res) => {
-  const product = await Product.findById(req.params.id);
+  const product = await Product.findById(req.params.id)
 
   if (product) {
-    product.name = req.body.name || product.name;
-    product.user = req.user._id;
-    product.image = req.body.image || product.image;
-    product.description = req.body.description || product.description;
-    product.brand = req.body.brand || product.brand;
-    product.category = req.body.category || product.category;
-    product.capacity = req.body.capacity || product.capacity;
+    product.name = req.body.name || product.name
+    product.user = req.user._id
+    product.image = req.body.image || product.image
+    product.description = req.body.description || product.description
+    product.brand = req.body.brand || product.brand
+    product.category = req.body.category || product.category
+    product.capacity = req.body.capacity || product.capacity
     product.capacityDropdown =
-      req.body.capacityDropdown || product.capacityDropdown;
-    product.price = req.body.price || product.price;
-    product.countInStock = req.body.countInStock || product.countInStock;
+      req.body.capacityDropdown || product.capacityDropdown
+    product.price = req.body.price || product.price
+    product.countInStock = req.body.countInStock || product.countInStock
     product.countryOfOrigin =
-      req.body.countryOfOrigin || product.countryOfOrigin;
-    product.isPromo = req.body.isPromo;
-    product.isPublished = req.body.isPublished;
+      req.body.countryOfOrigin || product.countryOfOrigin
+    product.isPromo = req.body.isPromo
+    product.isPublished = req.body.isPublished
 
-    const updatedProduct = await product.save();
-    res.json(updatedProduct);
+    const updatedProduct = await product.save()
+    res.json(updatedProduct)
   } else {
-    res.status(404);
-    throw new Error("Product not found");
+    res.status(404)
+    throw new Error("Product not found")
   }
-});
+})
